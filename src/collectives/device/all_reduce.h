875e8a2c3be2c56e202861b6a61bb1a3dfd168b9
chhwang <ch.hwang128@gmail.com>
2021-12-16 17:51:53 +0900

    Timestamp all_reduce.

--- src/collectives/device/all_reduce.h
+++ src/collectives/device/all_reduce.h
@@ -8,9 +8,45 @@
 #include "collectives.h"
 #include "primitives.h"
 
+#define PRINT_SCHED_INFO  1   // 0:disable, 1:enable
+
+// Zixing's code.
+// Returns the value of CUDA's global nanosecond timer.
+static __device__ __inline__ uint64_t globalTimer64()
+{
+  // Due to a bug in CUDA's 64-bit globaltimer, the lower 32 bits can wrap
+  // around after the upper bits have already been read. Work around this by
+  // reading the high bits a second time. Use the second value to detect a
+  // rollover, and set the lower bits of the 64-bit "timer reading" to 0, which
+  // would be valid, it's passed over during the duration of the reading. If no
+  // rollover occurred, just return the initial reading.
+  volatile uint64_t first_reading;
+  volatile uint32_t second_reading;
+  uint32_t high_bits_first;
+  asm volatile("mov.u64 %0, %%globaltimer;" : "=l"(first_reading));
+  high_bits_first = first_reading >> 32;
+  asm volatile("mov.u32 %0, %%globaltimer_hi;" : "=r"(second_reading));
+  if (high_bits_first == second_reading) {
+    return first_reading;
+  }
+  // Return the value with the updated high bits, but the low bits set to 0.
+  return ((uint64_t) second_reading) << 32;
+}
+
+static __device__ __inline__ void printSchedInfo()
+{
+  if (threadIdx.x == 0 && blockIdx.x == 0) {
+    printf("[SchedInfo] NCCL Timestamp %lu\n", globalTimer64());
+  }
+  __syncthreads();
+}
+
 namespace {
   template<typename T, typename RedOp, typename Proto>
   __device__ __forceinline__ void runRing(ncclWorkElem *args) {
+#if (PRINT_SCHED_INFO == 1)
+    printSchedInfo();
+#endif // PRINT_SCHED_INFO
     const int tid = threadIdx.x;
     const int nthreads = args->nThreads;
     const int bid = args->coll.bid;
